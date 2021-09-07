87c3b01599600ec81c6c5cc32d01fc8873d16180
MahrukhJ <88167139+MahrukhJ@users.noreply.github.com>
2021-09-07 12:41:29 -0400

    Update Homework #2-Lab #1.R

--- Homework #2-Lab #1.R
+++ Homework #2-Lab #1.R
@@ -10,6 +10,15 @@ Oppositely, if the die was unfair, the probability that it would be judged as fa
 "Possible Protocol #2":
 
 #Specify a decision rule to judge if a die is fair or unfair.
-For a dice to be considered fair, out of 20 rolls, the frequency of a 6 should amount to 3-5. 
-(This is because if a dice is fair and given that the probability of rolling a 6 is 1/6, 20/6 is 3.3 repeating and 2 more than the average is considered normal, according to our group)
+For a dice to be considered fair, out of "n" rolls, the frequency of a 6 should amount to around "n/6", give or take a certain number of rolls depending on 'n'. 
+"Taking the example from HW#1, out of 20 rolls, a die will be regarded as fair if it lands on a 6, 3-7 times. 
+For a die to be judged as unfair, out of 20 rolls, a die should land on a 6, 8 or more times. The number 8 was chosen because two of our groupmates rolled a 6 on the control die, 7 times and we concluded that 1 more would be unusual for a fair die." 
  
+#If a fair die is rolled 20 times, what is the likely number of a 6 resulting?
+Following our decision rule, 20/6 is 3.3 repeating, therefore, the likely number of times for a 6 to result, out of 20 rolls, is 3-5. 
+#How unusual is it to get 1 more or 1 less than that?
+To determine if it is unusual to roll 1 more or 1 less than the likely number of a 6 resulting, depends on the number of rolls. Out of 20, it is not unusual to roll 1 more or 1 less than the likely. 
+#How unusual is it to get 2 more or 2 less than that?
+Given the previous reasoning, it is not unusual to get 2 more or 2 less than the likely number of a certan number rolling, out of 20 rolls. 
+#How unusual is it to get 3 more or 3 less than that?
+Out of 20 rolls, it is not unusual to get 3 more or 3 less than the likely number of times of rolling a 6 (although it is borderline!).
