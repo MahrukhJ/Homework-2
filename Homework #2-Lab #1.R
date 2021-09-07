01866fa0ea57b733d51e57951d11413065bfcf8e
MahrukhJ <88167139+MahrukhJ@users.noreply.github.com>
2021-09-07 00:12:11 -0400

    Update Homework #2-Lab #1.R

--- Homework #2-Lab #1.R
+++ Homework #2-Lab #1.R
@@ -2,9 +2,9 @@ Homework #2 - Lab #1
 
 Possible Protocol #1:
 
-After rolling the die once, the result was a 3, thus allowing us to conclude that the die is fair. 
-Since the dice is fair, the probability for it to be judged as unfair is 1/6. 
-#since the probability of rolling any number is 1/6th on a fair die. 
+After rolling the die once, the result was a 3, thus allowing us to conclude that the die is fair.
+Following this result, the probability for the die to be judged as unfair is 1/6. 
+#This is because the probability of rolling any number is 1/6th on a fair die and rolling a 6, would presume this die unfair. 
 Oppositely, if the die was unfair, the probability that it would be judged as fair is 5/6. 
 
 Possible Protocol #2:
