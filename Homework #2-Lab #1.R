f7eae23dccffbf22f834ad3bb64823fda77600d1
MahrukhJ <88167139+MahrukhJ@users.noreply.github.com>
2021-09-07 16:08:44 -0400

    Update Homework #2-Lab #1.R

--- Homework #2-Lab #1.R
+++ Homework #2-Lab #1.R
@@ -22,3 +22,43 @@ To determine if it is unusual to roll 1 more or 1 less than the likely number of
 Given the previous reasoning, it is not unusual to get 2 more or 2 less than the likely number of a certan number rolling, out of 20 rolls. 
 #How unusual is it to get 3 more or 3 less than that?
 Out of 20 rolls, it is not unusual to get 3 more or 3 less than the likely number of times of rolling a 6 (although it is borderline!).
+
+"Analyze PP2":
+If the dice were fair, what is the chance that it could be judged as unfair?
+
+P(6) = P(Unfair) = 1/6
+P(1-5) = P(Fair) = 5/6
+
+If the dice is fair, the probability of rolling any number is 1/6 therefore, out of 20 rolls, for a dice to be judged unfair, the frequency of a 6 needs to be 8 or greater, i.e. greater than or equal to 40% (#based on line 15).
+ 
+"Possible Protocol #3":
+ 
+#specify decision rules after rolling the die 100 times. 
+The decision rules stay relevantly similar to PP2, "For a dice to be considered fair, out of "n" rolls, the frequency of a 6 should amount to around "n/6", give or take a certain number of rolls depending on 'n'."
+In this case, for a dice to be considered fair, out of 100 rolls, the frequency of a 6 should amount to around 15-20, since 100/6 is 16.6 repeating. 
+
+#Is it fair to say that every conclusion has some level of confidence attached? Where do you set boundaries for decisions?
+Confidence comes from the math! The decision rules are based on the formula "n/6", where "n" represents the number of rolls and 6 portrays the number of faces of the die. Dividing the number of rolls by the number of faces gives us the average number of rolls per face. 
+Although we receive the average number of rolls per face, that does not mean that out of 100 rolls, eace number will appear 16-17 times. Deviations are normal, so 5 more or less than the likely is not unusual. 
+ 
+"Analyze PP3":
+#What is the chance that fair dice could be judged to be unfair?
+ 
+P(6) = P(Unfair) = 100/6 = 16.66667 
+P(1-5) = P(Fair) = 5(100/6) = 83.33334
+ 
+For a fair die to be judged as unfair, out of 100 rolls, the frequency of a 6 needs to be an unusual amount greater than 16. Given the rationale in PP2, the unsusual amount is greater than or equal to 40%, i.e. the frequency of a 6 should be greater than or equal to 40 rolls, in order for the die to be considered unfair. 
+However, given the increased number of rolls, rolling twice the likely amount, i.e. 28 rolls, is basis for consideration as an unfair die.  
+
+
+"Experiment Protocol #1":
+(Without looking at data)
+#What is a reasonable number of times to roll your experiment dice?
+A reasonable number of times to roll the experiment die is 20 and the experiment can be repeated 2-3 times, amounting to a total of 40 or 60 rolls. 
+#If you roll the experiment dice a certain number of times and see a particular outcome, then you would conclude it is fair or not. How confident would you be, in any of those conclusions?
+Confidence from conclusions is a result of trust in the math and personal judgement. If we roll the dice a certain number of times and witness a particular outcome, judgement of fairness or unfairness becomes relatively indubious. 
+#Note that while previous Possible Protocols emphasized counting just the number of times a 6 came up, you might consider other outcomes such as the average value of the dice when rolled or the distribution of other outcomes (what number is on opposite face from 6? Do you suppose that number might be disproportionately represented if dice were not fair?).
+It certainly is a point of interest to record and analyze all values of the rolls and specifically observe the mode and the frequency of 1 (giving importance to 1 since it is the opposite face from 6 and if the rolls generate an unusual number of 1s, an explanation can be constructed). 
+
+                                                                                                                                            
+                                                                                                                                            
