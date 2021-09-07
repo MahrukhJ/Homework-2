5a8ac12ce7cb0d02b3ade2d638142a28b54b3ec7
Mahrukh A. Jaura <mahrukh.jaura@Mahrukhs-Air.home>
2021-09-07 00:08:50 -0400

    Commit!

--- Homework #2-Lab #1.R
+++ Homework #2-Lab #1.R
@@ -1,7 +1,11 @@
 Homework #2 - Lab #1
 
-<<<<<<< HEAD
-Possible #1(PP1):
-=======
 Possible Protocol #1:
->>>>>>> a3fb34abc9c8b4e5a80b138d5eb6d08e1c382f20
+
+After rolling the die once, the result was a 3, thus allowing us to conclude that the die is fair. 
+Since the dice is fair, the probability for it to be judged as unfair is 1/6. 
+#since the probability of rolling any number is 1/6th on a fair die. 
+Oppositely, if the die was unfair, the probability that it would be judged as fair is 5/6. 
+
+Possible Protocol #2:
+
