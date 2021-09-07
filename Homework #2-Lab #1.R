5afabf17ca565d2c54d904927f2a73657524eba4
MahrukhJ <88167139+MahrukhJ@users.noreply.github.com>
2021-09-07 00:21:02 -0400

    Update Homework #2-Lab #1.R

--- Homework #2-Lab #1.R
+++ Homework #2-Lab #1.R
@@ -1,11 +1,15 @@
 Homework #2 - Lab #1
 
-Possible Protocol #1:
+"Possible Protocol #1":
 
 After rolling the die once, the result was a 3, thus allowing us to conclude that the die is fair.
 Following this result, the probability for the die to be judged as unfair is 1/6. 
 #This is because the probability of rolling any number is 1/6th on a fair die and rolling a 6, would presume this die unfair. 
 Oppositely, if the die was unfair, the probability that it would be judged as fair is 5/6. 
 
-Possible Protocol #2:
+"Possible Protocol #2":
 
+#Specify a decision rule to judge if a die is fair or unfair.
+For a dice to be considered fair, out of 20 rolls, the frequency of a 6 should amount to 3-5. 
+(This is because if a dice is fair and given that the probability of rolling a 6 is 1/6, 20/6 is 3.3 repeating and 2 more than the average is considered normal, according to our group)
+ 
